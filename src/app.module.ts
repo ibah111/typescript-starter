b8f9e749223de8dc8d6d95cd3f298cdacadb5391
Ibah <starlord77@mail.ru>
2024-02-22 13:58:23 +0300

    fix: added module of modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
-import { ModuleOfPages } from './Pages';
+import { ModuleOfPages } from './Pages/Pages.module';
+import { ModuleOfModules } from './Modules';
 
 @Module({
-  providers: [ModuleOfPages, ModuleOfPages],
+  imports: [ModuleOfPages, ModuleOfModules],
 })
 export class AppModule {}
